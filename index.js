e9ebb241f4dc8ffa59fdfa9d6c5ca02a62145de2
xiongwilee <xiongwilee@foxmail.com>
2016-04-04 15:09:16 +0800

    update: by wilee

--- index.js
+++ index.js
@@ -5,6 +5,94 @@ const fs = require('fs');
 const router = require('koa-router');
 const debug = require('debug')('koa-grace:router');
 
+const Router = router();
+
+/**
+ * 生成路由控制
+ * @param  {string} app     context
+ * @param  {object} options 配置项
+ *         {string} options.root controller路径
+ *         {string} options.default_path 默认路径
+ *         {string} options.domain 请求域名,可不传
+ * @return {function}       
+ */
+function graceRouter(app, options) {
+  const Domain = options.domain || '';
+
+  // app的默认路由
+  if (options.default_path) {
+    Router.redirect('/', options.default_path);
+  }
+
+  if (typeof options === 'string') {
+    options = {
+      root: options
+    };
+  } else if (!options || !options.root) {
+    throw new Error('`root` config required.');
+  }
+
+  let root = options.root;
+  _ls(root).forEach(function(filePath) {
+    if (!/([a-zA-Z0-9_\-]+)(\.js)$/.test(filePath)) {
+      return;
+    }
+
+    let exportFuncs = require(filePath);
+    let pathRegexp = _formatPath(filePath, root);
+
+    // 如果当前设置了不是路由，则暂停
+    if (exportFuncs.__controller__ === false) {
+      return;
+    }
+
+    let ctrlMethod = exportFuncs.__method__;
+    let ctrlRegular = exportFuncs.__regular__;
+
+    if (typeof exportFuncs === 'function') {
+      _setRoute(pathRegexp, {
+        domain: Domain,
+        method: ctrlMethod,
+        ctrl: exportFuncs,
+        regular: ctrlRegular
+      });
+    } else {
+      for (let ctrlname in exportFuncs) {
+        if (typeof exportFuncs[ctrlname] !== 'function') {
+          continue;
+        }
+
+        _setRoute(pathRegexp, {
+          domain: Domain,
+          method: exportFuncs[ctrlname].__method__ || ctrlMethod,
+          regular: exportFuncs[ctrlname].__regular__ || ctrlRegular,
+          ctrlname: ctrlname,
+          ctrl: exportFuncs[ctrlname]
+        });
+      };
+    }
+
+    // app.use(Router.routes());
+  });
+
+  // 添加bindDefault方法
+  let deafaultCtrlRoot = options.deafaultCtrlRoot || options.root;
+  deafaultCtrlRoot += '/defaultCtrl';
+  app.context.__defineGetter__("bindDefault", function() {
+    return require(deafaultCtrlRoot);
+  });
+
+  return function* graceRouter(next) {
+    
+    debug(this.request.url);
+
+    yield Router.routes();
+
+    yield next;
+  };
+};
+
+
 /**
  * 查找目录中的所有文件
  * @param  {string} dir       查找路径
@@ -59,114 +147,36 @@ function _formatPath(filePath, root) {
 }
 
 /**
- * 生成路由控制
- * @param  {string} app     context
- * @param  {object} options 配置项
- *         {string} options.root controller路径
- *         {string} options.default_path 默认路径
- *         {string} options.domain 请求域名,可不传
- * @return {function}       
+ * 设置路由
+ * @param {string} path 当前文件路径
+ * @param {object} opt  配置项
+ *        {string} opt.method 当前请求方法：get,post等
+ *        {string} opt.regular 参考：https://github.com/alexmingoia/koa-router#nested-routers
+ *        {string} opt.ctrlname 当前controller名称
+ *        {funtion} opt.ctrl controller方法
  */
-function hornbillRouter(app, options) {
-  const Router = router();
-  const Domain = options.domain || '';
+function _setRoute(path, opt) {
+  let paths = [];
+  let method = opt.method || 'get';
+  let pathname = opt.ctrlname ? (path + '/' + opt.ctrlname) : path;
 
-  /**
-   * 设置路由
-   * @param {string} path 当前文件路径
-   * @param {object} opt  配置项
-   *        {string} opt.method 当前请求方法：get,post等
-   *        {string} opt.regular 参考：https://github.com/alexmingoia/koa-router#nested-routers
-   *        {string} opt.ctrlname 当前controller名称
-   *        {funtion} opt.ctrl controller方法
-   */
-  function _setRoute(path, opt) {
-    let paths = [];
-    let method = opt.method || 'get';
-    let pathname = opt.ctrlname ? (path + '/' + opt.ctrlname) : path;
+  // 真实路由
+  paths.push(pathname);
 
-    // 真实路由
-    paths.push(pathname);
+  // 如果配置了regular则配置regular路由，例如：/post的regular为/:id,则添加一个/post/:id的路由
+  opt.regular && paths.push(pathname + opt.regular);
 
-    // 如果配置了regular则配置regular路由，例如：/post的regular为/:id,则添加一个/post/:id的路由
-    opt.regular && paths.push(pathname + opt.regular);
+  // 如果是index就是当前路径的默认路由
+  opt.ctrlname === 'index' && paths.push(path);
 
-    // 如果是index就是当前路径的默认路由
-    opt.ctrlname === 'index' && paths.push(path);
+  // 注入路由
+  paths.forEach(function(pathItem) {
+    debug(method + ':' + opt.Domain + pathItem);
 
-    // 注入路由
-    paths.forEach(function(pathItem) {
-      debug(method + ':' + Domain + pathItem);
-      Router[method](pathItem, opt.ctrl);
-    });
-
-    return;
-  }
-
-  // app的默认路由
-  if (options.default_path) {
-    Router.redirect('/', options.default_path);
-  }
-
-  if (typeof options === 'string') {
-    options = {
-      root: options
-    };
-  } else if (!options || !options.root) {
-    throw new Error('`root` config required.');
-  }
-
-  let root = options.root;
-  _ls(root).forEach(function(filePath) {
-    if (!/([a-zA-Z0-9_\-]+)(\.js)$/.test(filePath)) {
-      return;
-    }
-
-    let exportFuncs = require(filePath);
-    let pathRegexp = _formatPath(filePath, root);
-
-    // 如果当前设置了不是路由，则暂停
-    if (exportFuncs.__controller__ === false) {
-      return;
-    }
-
-    let ctrlMethod = exportFuncs.__method__;
-    let ctrlRegular = exportFuncs.__regular__;
-
-    if (typeof exportFuncs === 'function') {
-      _setRoute(pathRegexp, {
-        method: ctrlMethod,
-        ctrl: exportFuncs,
-        regular: ctrlRegular
-      });
-    } else {
-      for (let ctrlname in exportFuncs) {
-        if (typeof exportFuncs[ctrlname] !== 'function') {
-          continue;
-        }
-
-        _setRoute(pathRegexp, {
-          method: exportFuncs[ctrlname].__method__ || ctrlMethod,
-          regular: exportFuncs[ctrlname].__regular__ || ctrlRegular,
-          ctrlname: ctrlname,
-          ctrl: exportFuncs[ctrlname]
-        });
-      };
-    }
-
-    app.use(Router.routes());
+    Router[method](pathItem, opt.ctrl);
   });
 
-  // 添加bindDefault方法
-  let deafaultCtrlRoot = options.deafaultCtrlRoot || options.root;
-  deafaultCtrlRoot += '/defaultCtrl';
-  app.context.__defineGetter__("bindDefault", function() {
-    return require(deafaultCtrlRoot);
-  });
+  return;
+}
 
-  return function* hornbillRouter(next) {
-    yield next;
-  };
-};
-
-module.exports = hornbillRouter
+module.exports = graceRouter
